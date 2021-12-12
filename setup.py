f460485ba01244f95331fba152b38c42029c7adb
Anton Geraschenko <geraschenko@gmail.com>
2021-12-11 18:22:19 -0800

    Add setup.py and requirements.txt.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='toric',
+   version='0.0',
+   description='Python utilities for dealing with toric monoids and diagrams of toric monoids.',
+   author='Anton Geraschenko',
+   author_email='geraschenko@gmail.com',
+   packages=['toric'],
+)
